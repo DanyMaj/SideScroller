7a6b2cdd10a923c95bd3cfa2beb4aea31e239813
DanyMaj <d.majani@rns.ecolescreatives.com>
2026-02-05 18:10:07 +0100

    v1

---
+++ Assets/Script/PlayerMovement_01.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerMovement_01: MonoBehaviour
+{
+    public Rigidbody2D rb; //Ne pas oublier d'activer la gravity scale du rigidbody et d'ajouter un collider
+    public float speed;
+    public float jumpforce;
+    public LayerMask mask; //Quels layer seront affecté par le raycast attention a ne pas ajouter le layer de votre perso sinon le raycast va trouver le perso avant de trouver le sol
+
+    void Update()
+    {
+        var hDirection = 0f;
+        var vDirection = 0f;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (CheckGround())
+            {
+                vDirection += jumpforce;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            hDirection += -1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            hDirection += 1;
+        }
+
+        rb.linearVelocity = new Vector2(hDirection * speed, rb.linearVelocityY + vDirection); //On set up la velocité horizontal 
+    }
+
+    public bool CheckGround()
+    {
+        var rayCastHit = Physics2D.Raycast(transform.position, new Vector2(0, -1), 1.1f, mask);
+        if (rayCastHit)
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
