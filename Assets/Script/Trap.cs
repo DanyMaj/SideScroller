f73f8924b8ba18bc549e3c97c492f9718e95f139
DanyMaj <louisbequin2@gmail.com>
2026-02-12 18:09:11 +0100

    s

---
+++ Assets/Script/Trap.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Trap : Interactable
+{
+    public int dammage = 3;
+    public override void Interaction()
+    {
+        var healthManager = player.GetComponent<HealthManager>();
+        healthManager.AddHp(-dammage);
+        healthManager.GetHP();
+        print("teste");
+    }
+}
